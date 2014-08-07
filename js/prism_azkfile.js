4d1a3b2fc64800ee404f13fefefaabe776944bf1
Everton Ribeiro <nuxlli@gmail.com>
2014-08-07 12:00:50 -0300

    Replacing prettify for prism and fix Azkfile.js in xs.

---
+++ js/prism_azkfile.js
@@ -0,0 +1,11 @@
+Prism.languages.azkfile = Prism.languages.extend('clike', {
+	'keyword': /\b(system|systems|break|case|catch|class|const|continue|debugger|delete|do|else|enum|export|extends|false|finally|for|function|get|if|implements|import|in|instanceof|interface|let|new|null|package|private|protected|public|return|set|static|super|switch|this|throw|true|try|typeof|var|void|while|with|yield)\b/g,
+	'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?|NaN|-?Infinity)\b/g
+});
+
+Prism.languages.insertBefore('azkfile', 'keyword', {
+	'regex': {
+		pattern: /(^|[^/])\/(?!\/)(\[.+?]|\\.|[^/\r\n])+\/[gim]{0,3}(?=\s*($|[\r\n,.;})]))/g,
+		lookbehind: true
+	}
+});
