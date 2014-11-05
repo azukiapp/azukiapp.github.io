fbc4484e513bd4530149b7b366a6fb8a0e9ead2b
Julio Makdisse Saito <saitodisse@gmail.com>
2014-11-05 12:38:32 -0200

    Changes cache expiring date to 2yr from now.

--- Azkfile.js
+++ Azkfile.js
@@ -8,29 +8,31 @@ systems({
     // Dependent systems
     depends: [],
     // More images:  http://images.azk.io
-    image: "kisenka/centos6-jekyll",
+    image: 'kisenka/centos6-jekyll',
     // Steps to execute before running instances
     provision: [
-      "npm install",
-      "bundle install --path /azk/bundler",
+      'npm install',
+      'bundle install --path /azk/bundler',
     ],
-    command: "bundle exec jekyll serve -s ./src/ --port=$HTTP_PORT --watch --force_polling",
-    workdir: "/azk/#{manifest.dir}",
+    command: 'bundle exec jekyll serve -s ./src/ --port=$HTTP_PORT --watch --force_polling',
+    workdir: '/azk/#{manifest.dir}',
     // Mounts folders to assigned paths
     mounts: {
-      "/azk/#{manifest.dir}": ".",
-      '/azk/bundler'        : persistent('bundler'),
+      '/azk/#{manifest.dir}'             : '.',
+      '/azk/#{manifest.dir}/_site'       : persistent('_site'),
+      '/azk/#{manifest.dir}/node_modules': persistent('node_modules'),
+      '/azk/bundler'                     : persistent('bundler'),
     },
     http: {
-      domains: [ "#{system.name}.#{azk.default_domain}" ]
+      domains: [ '#{system.name}.#{azk.default_domain}' ]
     },
     ports: {
-      http: "4000/tcp",
+      http: '4000/tcp',
     },
     envs: {
-      RUBY_ENV : "development",
-      PATH: "/azk/#{manifest.dir}/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-      BUNDLE_APP_CONFIG : "/azk/bundler",
+      RUBY_ENV : 'development',
+      PATH: '/azk/#{manifest.dir}/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      BUNDLE_APP_CONFIG : '/azk/bundler',
     }
   },
 });
