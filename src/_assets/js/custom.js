cd0ed62f442f929740a15ba51d9848317a267389
Julio Makdisse Saito <saitodisse@gmail.com>
2014-11-05 18:33:25 -0200

    Fixing navbar links

--- src/_assets/js/custom.js
+++ src/_assets/js/custom.js
@@ -1,16 +1,30 @@
 //Nav
 jQuery(document).ready(function ($){
     jQuery('.navbar .nav > li > a, .footer-nav > li > a').click(function(event){
-    	var href = $.attr(this, 'href');
+        var href = $.attr(this, 'href');
         jQuery.scrollTo( $(this).attr("href"), {
-			duration: 1000,
-			easing:'easeInOutExpo',
-			onAfter: function() { window.location.hash = href; }
-		});
-		return false;
+            duration: 700,
+            easing:'easeInOutExpo',
+            onAfter: function() {
+                window.location.hash = href;
+
+                //inactive all and put activeclass on the right place
+                jQuery('li.active').removeClass('active');
+                jQuery(event.target).parent().addClass('active');
+            }
+        });
+
+
+        return false;
     });
 
-    $('body').scrollspy({ target: '.navbar-collapse' });
+    if(document.location.hash !== ''){
+        jQuery('li.active').removeClass('active');
+        var selectedAnchor = jQuery('a[href="'+ document.location.hash +'"]:eq(0)');
+        selectedAnchor.parent().addClass('active');
+    }
+
+    // $('body').scrollspy({ target: '.navbar-collapse' });
 });
 
 var tpj=jQuery;
@@ -18,54 +32,55 @@ tpj.noConflict();
 
 tpj(document).ready(function() {
 
-if (tpj.fn.cssOriginal!=undefined)
-tpj.fn.css = tpj.fn.cssOriginal;
+    if (tpj.fn.cssOriginal!=undefined){
+        tpj.fn.css = tpj.fn.cssOriginal;
+    }
 
-tpj('.banner').revolution(
-	{
-		delay:5000,
-		startheight:520,
-		startwidth:1170,
-
-
-		hideThumbs:200,
-
-		thumbWidth:100,							// Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
-		thumbHeight:50,
-		thumbAmount:5,
-
-		navigationType:"bullet",				// bullet, thumb, none
-		navigationArrows:"nexttobullets",				// nexttobullets, solo (old name verticalcentered), none
-
-		navigationStyle:"round-old",				// round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
+    tpj('.banner').revolution({
+        delay:5000,
+        startheight:520,
+        startwidth:1170,
 
 
-		navigationHAlign:"center",				// Vertical Align top,center,bottom
-		navigationVAlign:"bottom",					// Horizontal Align left,center,right
-		navigationHOffset:0,
-		navigationVOffset:-30,
+        hideThumbs:200,
 
-		soloArrowLeftHalign:"left",
-		soloArrowLeftValign:"top",
-		soloArrowLeftHOffset:20,
-		soloArrowLeftVOffset:0,
+        thumbWidth:100,                         // Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
+        thumbHeight:50,
+        thumbAmount:5,
 
-		soloArrowRightHalign:"right",
-		soloArrowRightValign:"top",
-		soloArrowRightHOffset:20,
-		soloArrowRightVOffset:0,
+        navigationType:"bullet",                // bullet, thumb, none
+        navigationArrows:"nexttobullets",               // nexttobullets, solo (old name verticalcentered), none
 
-		touchenabled:"on",						// Enable Swipe Function : on/off
-		onHoverStop:"off",						// Stop Banner Timet at Hover on Slide on/off
+        navigationStyle:"round-old",                // round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
 
-		stopAtSlide:-1,							// Stop Timer if Slide "x" has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
-		stopAfterLoops:-1,						// Stop Timer if All slides has been played "x" times. IT will stop at THe slide which is defined via stopAtSlide:x, if set to -1 slide never stop automatic
 
-		hideCaptionAtLimit:0,					// It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
-		hideAllCaptionAtLilmit:0,				// Hide all The Captions if Width of Browser is less then this value
-		hideSliderAtLimit:0,					// Hide the whole slider, and stop also functions if Width of Browser is less than this value
+        navigationHAlign:"center",              // Vertical Align top,center,bottom
+        navigationVAlign:"bottom",                  // Horizontal Align left,center,right
+        navigationHOffset:0,
+        navigationVOffset:-30,
 
-		shadow:1,								//0 = no Shadow, 1,2,3 = 3 Different Art of Shadows  (No Shadow in Fullwidth Version !)
-		fullWidth:"off"							// Turns On or Off the Fullwidth Image Centering in FullWidth Modus
-	});
+        soloArrowLeftHalign:"left",
+        soloArrowLeftValign:"top",
+        soloArrowLeftHOffset:20,
+        soloArrowLeftVOffset:0,
+
+        soloArrowRightHalign:"right",
+        soloArrowRightValign:"top",
+        soloArrowRightHOffset:20,
+        soloArrowRightVOffset:0,
+
+        touchenabled:"on",                      // Enable Swipe Function : on/off
+        onHoverStop:"off",                      // Stop Banner Timet at Hover on Slide on/off
+
+        stopAtSlide:-1,                         // Stop Timer if Slide "x" has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
+        stopAfterLoops:-1,                      // Stop Timer if All slides has been played "x" times. IT will stop at THe slide which is defined via stopAtSlide:x, if set to -1 slide never stop automatic
+
+        hideCaptionAtLimit:0,                   // It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
+        hideAllCaptionAtLilmit:0,               // Hide all The Captions if Width of Browser is less then this value
+        hideSliderAtLimit:0,                    // Hide the whole slider, and stop also functions if Width of Browser is less than this value
+
+        shadow:1,                               //0 = no Shadow, 1,2,3 = 3 Different Art of Shadows  (No Shadow in Fullwidth Version !)
+        fullWidth:"off"                         // Turns On or Off the Fullwidth Image Centering in FullWidth Modus
+    });
+
 });
