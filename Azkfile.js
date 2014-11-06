e9a6d8f736dc2acadca104f47148a8907f6bd876
Julio Makdisse Saito <saitodisse@gmail.com>
2014-11-06 17:01:17 -0200

    Adding folder as persistent folders

--- Azkfile.js
+++ Azkfile.js
@@ -7,7 +7,8 @@ systems({
     // Steps to execute before running instances
     provision: [
       'bundle install --path /azk/bundler',
-      'npm install'
+      'npm install',
+      './node_modules/bower/bin/bower --allow-root install',
     ],
     workdir : '/azk/#{manifest.dir}',
     command : 'bundle exec jekyll serve -s ./src/ --config ./src/_config.yml --port=$HTTP_PORT --watch --force_polling',
@@ -16,8 +17,12 @@ systems({
     scalable: {'default': 1},
     // Mounts folders to assigned paths
     mounts: {
-      '/azk/#{manifest.dir}': path('.'),
-      '/azk/bundler'        : persistent('bundler'),
+      '/azk/bundler'                         : persistent('bundler'),
+      '/azk/#{manifest.dir}'                 : path('.'),
+      '/azk/#{manifest.dir}/_site'           : persistent('_site'),
+      '/azk/#{manifest.dir}/bower_components': persistent('bower_components'),
+      '/azk/#{manifest.dir}/node_modules'    : persistent('node_modules'),
+      '/azk/#{manifest.dir}/build'           : persistent('build'),
     },
     http: {
       domains: ['#{system.name}.#{azk.default_domain}']
