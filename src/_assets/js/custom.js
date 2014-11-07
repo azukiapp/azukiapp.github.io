7959e8fac9392a79697675a6e0593bbde3cd75f6
Julio Makdisse Saito <saitodisse@gmail.com>
2014-11-07 10:54:56 -0200

    Closing responsive navbar #14

--- src/_assets/js/custom.js
+++ src/_assets/js/custom.js
@@ -1,12 +1,9 @@
 //Nav
 $(function($) {
+
   // when nav bar links clicked, highlight them
   var navBarOrFootLinks = $('.navbar .nav > li > a, .footer-nav > li > a');
   navBarOrFootLinks.click(function(event) {
-
-    event.preventDefault();
-    event.stopPropagation();
-
     var href = $.attr(this, 'href');
     $(href).scrollToBySpeed({
       speed: 5000,
@@ -15,12 +12,15 @@ $(function($) {
       // onAfter: function() {
       // }
     });
-
     history.pushState(null, null, href);
     //inactive all and put activeclass on the right place
     $('li.active').removeClass('active');
     $(event.target).parent().addClass('active');
 
+    $('.navbar-collapse').collapse('hide');
+
+    event.preventDefault();
+    event.stopPropagation();
     return false;
   });
 
@@ -48,17 +48,23 @@ tpj(document).ready(function() {
 
     hideThumbs             : 200,
 
-    thumbWidth             : 100,                         // Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
+    // Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
+    thumbWidth             : 100,
     thumbHeight            : 50,
     thumbAmount            : 5,
 
-    navigationType         : 'bullet',                // bullet, thumb, none
-    navigationArrows       : 'nexttobullets',               // nexttobullets, solo (old name verticalcentered), none
+    // bullet, thumb, none
+    navigationType         : 'bullet',
+    // nexttobullets, solo (old name verticalcentered), none
+    navigationArrows       : 'nexttobullets',
 
-    navigationStyle        : 'round-old',                // round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
+    // round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
+    navigationStyle        : 'round-old',
 
-    navigationHAlign       : 'center',              // Vertical Align top,center,bottom
-    navigationVAlign       : 'bottom',                  // Horizontal Align left,center,right
+    // Vertical Align top,center,bottom
+    navigationHAlign       : 'center',
+    // Horizontal Align left,center,right
+    navigationVAlign       : 'bottom',
     navigationHOffset      : 0,
     navigationVOffset      : -30,
 
@@ -72,18 +78,27 @@ tpj(document).ready(function() {
     soloArrowRightHOffset  : 20,
     soloArrowRightVOffset  : 0,
 
-    touchenabled           : 'on',                      // Enable Swipe Function : ' on'/off
-    onHoverStop            : 'off',                      // Stop Banner Timet at Hover on Slide on/off
+    // Enable Swipe Function : ' on'/off
+    touchenabled           : 'on',
+    // Stop Banner Timet at Hover on Slide on/off
+    onHoverStop            : 'off',
 
-    stopAtSlide            : -1,                         // Stop Timer if Slide x has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
-    stopAfterLoops         : -1,                      // Stop Timer if All slides has been played x times. IT will stop at THe slide which is defined via stopAtSlide: x, if set to -1 slide never stop automatic
+    // Stop Timer if Slide x has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
+    stopAtSlide            : -1,
+    // Stop Timer if All slides has been played x times. IT will stop at THe slide which is defined via stopAtSlide: x, if set to -1 slide never stop automatic
+    stopAfterLoops         : -1,
 
-    hideCaptionAtLimit     : 0,                   // It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
-    hideAllCaptionAtLilmit : 0,               // Hide all The Captions if Width of Browser is less then this value
-    hideSliderAtLimit      : 0,                    // Hide the whole slider, and stop also functions if Width of Browser is less than this value
+    // It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
+    hideCaptionAtLimit     : 0,
+    // Hide all The Captions if Width of Browser is less then this value
+    hideAllCaptionAtLilmit : 0,
+    // Hide the whole slider, and stop also functions if Width of Browser is less than this value
+    hideSliderAtLimit      : 0,
 
-    shadow                 : 1,                               //0 = no Shadow, 1,2,3 = 3 Different Art of Shadows  (No Shadow in Fullwidth Version !)
-    fullWidth              : 'off'                         // Turns On or Off the Fullwidth Image Centering in FullWidth Modus
+    //0 = no Shadow, 1,2,3 = 3 Different Art of Shadows  (No Shadow in Fullwidth Version !)
+    shadow                 : 1,
+    // Turns On or Off the Fullwidth Image Centering in FullWidth Modus
+    fullWidth              : 'off'
   });
 
 });
