919bacbdebea9ce14bde034df0bb722409422926
Everton Ribeiro <nuxlli@gmail.com>
2015-02-01 01:13:49 -0200

    Adding path and persistent to keywords

--- src/_assets/js/prism_azkfile.js
+++ src/_assets/js/prism_azkfile.js
@@ -1,5 +1,5 @@
 Prism.languages.azkfile = Prism.languages.extend('clike', {
-	'keyword': /\b(system|systems|break|case|catch|class|const|continue|debugger|delete|do|else|enum|export|extends|false|finally|for|function|get|if|implements|import|in|instanceof|interface|let|new|null|package|private|protected|public|return|set|static|super|switch|this|throw|true|try|typeof|var|void|while|with|yield)\b/g,
+	'keyword': /\b(path|persistent|system|systems|break|case|catch|class|const|continue|debugger|delete|do|else|enum|export|extends|false|finally|for|function|get|if|implements|import|in|instanceof|interface|let|new|null|package|private|protected|public|return|set|static|super|switch|this|throw|true|try|typeof|var|void|while|with|yield)\b/g,
 	'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?|NaN|-?Infinity)\b/g
 });
 
