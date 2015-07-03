f15e7f52eb150d62e187bf9d3c380eb025e0b3c2
Julio Makdisse Saito <saitodisse@gmail.com>
2015-07-03 19:02:37 -0300

    Azkfile with sync

--- Azkfile.js
+++ Azkfile.js
@@ -3,7 +3,7 @@ systems({
 
     depends: [],
 
-    image: {'docker': 'azukiapp/ruby'},
+    image: {'docker': 'azukiapp/ruby:2.2'},
 
     provision: [
       'bundle install --path /azk/bundler',
@@ -19,16 +19,13 @@ systems({
 
     mounts: {
       '/azk/bundler'                         : persistent('#{manifest.dir}/bundler'),
-      '/azk/#{manifest.dir}'                 : path('.'),
+      // see this link if you get an error on sync
+      // http://stackoverflow.com/questions/16748737/grunt-watch-error-waiting-fatal-error-watch-enospc
+      '/azk/#{manifest.dir}'                 : sync('.'),
       '/azk/#{manifest.dir}/_site'           : path('#{manifest.dir}/_site'),
       '/azk/#{manifest.dir}/bower_components': path('#{manifest.dir}/bower_components'),
       '/azk/#{manifest.dir}/node_modules'    : path('#{manifest.dir}/node_modules'),
       '/azk/#{manifest.dir}/build'           : path('#{manifest.dir}/build'),
-      // '/azk/#{manifest.dir}'         : sync('.'),
-      // '/azk/bundler'                 : persistent('#{manifest.dir}/bundler'),
-      // '/azk/#{manifest.dir}/tmp'     : persistent('#{manifest.dir}/tmp'),
-      // '/azk/#{manifest.dir}/log'     : path('#{manifest.dir}/log'),
-      // '/azk/#{manifest.dir}/.bundle' : path('#{manifest.dir}/.bundle'),
     },
 
     scalable: {'default': 1},
